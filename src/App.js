31434fdd7ee6f45f01421797e053139112a5d9e6
Inder Bir Singh <inderbirsingh03@gmail.com>
2021-03-25 00:20:49 +0530

    Data Fetched From Api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import React,{useState,useEffect} from 'react';
 import './App.css';
-
+import Header from './components/ui/Header';
+import axios from 'axios'
 function App() {
+
+  const [items,setItems] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
+
+  useEffect(() =>{
+
+    const fetchItems = async() =>{
+      const result = await axios.get(`https://www.breakingbadapi.com/api/characters`);
+
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    }
+    fetchItems();
+
+  },[]);
+
   return (
     <div className="container">
+     <Header/>
       
     </div>
   );
---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../img/logo.png'
+
+const Header = () => {
+    return (
+        <header className="center">
+            <img src={logo}></img>
+            
+        </header>
+    );
+}
+
+export default Header;
